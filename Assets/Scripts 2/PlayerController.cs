1ea1bf83a184f8e4785603f655a1916a6c7c4933
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-27 15:14:06 +0900

    手順８

--- Assets/Scripts 2/PlayerController.cs
+++ Assets/Scripts 2/PlayerController.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private string horizontal = "Horizontal";    // キー入力用の文字列指定
-    private string jump = "Jump";                // キー入力用の文字列指定
-
+    private string jump = "Jump";
 
     private Rigidbody2D rb;                      // コンポーネントの取得用
+
     private float scale;                         // 向きの設定に利用する
 
 
     private Animator anim;
 
-    public float moveSpeed;                      // 移動速度
-    public float jumpPower;                     // ジャンプ・浮遊力
+    private float limitPosX = 8.2f;           // 横方向の制限値
+    private float limitPosY = 4.45f;          // 縦方向の制限値
 
-    ////* ここから追加 *////
+    ////* ここまで *////
+
+
+    public float moveSpeed;                      // 移動速度
+    public float jumpPower;                      // ジャンプ・浮遊力
 
     public bool isGrounded;
 
     [SerializeField, Header("Linecast用 地面判定レイヤー")]
     private LayerMask groundLayer;
-
-    ////* ここまで *////
-
 
 
     void Start()
@@ -42,15 +42,14 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
-
         // 地面接地  Physics2D.Linecastメソッドを実行して、Ground Layerとキャラのコライダーとが接地している距離かどうかを確認し、接地しているなら true、接地していないなら false を戻す
         isGrounded = Physics2D.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, groundLayer);
+
         // Sceneビューに Physics2D.LinecastメソッドのLineを表示する
         Debug.DrawLine(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, Color.red, 1.0f);
 
         // ジャンプ
-        if (Input.GetButtonDown(jump))      // InputManager の Jump の項目に登録されているキー入力を判定する
+        if (Input.GetButtonDown(jump))
         {
             Jump();
         }
@@ -61,25 +60,28 @@ public class PlayerController : MonoBehaviour
             // 落下アニメを繰り返す
             anim.SetTrigger("Fall");
         }
-
     }
+
     /// <summary>
     /// ジャンプと空中浮遊
     /// </summary>
-
     private void Jump()
     {
+
         // キャラの位置を上方向へ移動させる(ジャンプ・浮遊)
         rb.AddForce(transform.up * jumpPower);
 
         // Jump(Up + Mid) アニメーションを再生する
         anim.SetTrigger("Jump");
     }
+
+
     void FixedUpdate()
     {
         // 移動
         Move();
     }
+
 
     /// <summary>
     /// 移動
@@ -120,12 +122,9 @@ public class PlayerController : MonoBehaviour
             // キャラの向きを移動方向に合わせる
             transform.localScale = temp;
 
-
-            ////* ここから追加 *////
-
             // 待機状態のアニメの再生を止めて、走るアニメの再生への遷移を行う
-            anim.SetBool("Idle", false);   // ☆　追加　Idle アニメーションを false にして、待機アニメーションを停止する
-            anim.SetFloat("Run", 0.5f);    // ☆　追加  Run アニメーションに対して、0.5f の値を情報として渡す。遷移条件が greater 0.1 なので、0.1 以上の値を渡すと条件が成立してRun アニメーションが再生される
+            anim.SetBool("Idle", false);
+            anim.SetFloat("Run", 0.5f);
 
         }
         else
@@ -134,12 +133,26 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(0, rb.velocity.y);
 
             //  走るアニメの再生を止めて、待機状態のアニメの再生への遷移を行う
-            anim.SetFloat("Run", 0.0f);     // ☆　追加  Run アニメーションに対して、0.f の値を情報として渡す。遷移条件が less 0.1 なので、0.1 以下の値を渡すと条件が成立してRun アニメーションが停止される
-            anim.SetBool("Idle", true);     // ☆　追加　Idle アニメーションを true にして、待機アニメーションを再生する
-
-            ////* ここまで追加 *////
-
+            anim.SetFloat("Run", 0.0f);
+            anim.SetBool("Idle", true);
         }
+
+
+        ////* ここから追加 *////
+
+        // 現在の位置情報が移動範囲の制限範囲を超えていないか確認する。超えていたら、制限範囲内に収める
+        float posX = Mathf.Clamp(transform.position.x, -limitPosX, limitPosX);
+        float posY = Mathf.Clamp(transform.position.y, -limitPosY, limitPosY);
+
+        // 現在の位置を更新(制限範囲を超えた場合、ここで移動の範囲を制限する)
+        transform.position = new Vector2(posX, posY);
+
+
+        ////* ここまで *////
+
     }
+
+
+
 
 }
